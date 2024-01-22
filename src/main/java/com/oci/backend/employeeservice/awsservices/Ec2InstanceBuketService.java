2dbeb24044e4f802a220fb13e8028833a57fcd94
Anil Acharya <anil.acharya6851@gmail.com>
2024-01-21 19:19:52 -0500

    make it runnable in ec2

---
+++ src/main/java/com/oci/backend/employeeservice/awsservices/Ec2InstanceBuketService.java
@@ -0,0 +1,24 @@
+package com.oci.backend.employeeservice.awsservices;
+
+import com.oci.backend.employeeservice.configurations.S3BucketConfig;
+import com.oci.backend.employeeservice.models.User;
+import com.oci.backend.employeeservice.utils.MapperUtils;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+
+import java.util.Set;
+
+public class Ec2InstanceBuketService implements  BucketFileDownload<Set<User>>{
+    @Override
+    public Set<User> downloadFile(S3BucketConfig s3BucketConfig) {
+        AwsCredentialsProvider provider = InstanceProfileCredentialsProvider.create();
+        S3Client s3Client =   S3Client.builder().credentialsProvider(provider).region(Region.US_EAST_1).build();
+        GetObjectRequest objectRequest = GetObjectRequest.builder().bucket(s3BucketConfig.getBucketName()).key(s3BucketConfig.getFileName()).build();
+        var response = s3Client.getObjectAsBytes(objectRequest);
+        var data = response.asByteArray();
+        return MapperUtils.readAll(data);
+    }
+}
